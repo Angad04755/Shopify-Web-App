cf51e384da36faf116e034a872b20721bd14b9b0
Angad <angad04755@gmail.com>
2026-09-02 12:21:30 +0530

    changes

--- src/components/wishlist/WishlistPage.tsx
+++ src/components/wishlist/WishlistPage.tsx
@@ -11,11 +11,11 @@ function WishlistPage() {
     useEffect(() => {
         window.scrollTo({ top: 0, behavior: "instant"})
     }, []);
-    
+
     if (items.length === 0) {
         return (
             <main className="min-h-screen bg-gray-50 flex justify-center items-center">
-                <section className="mx-auto container p-5 flex flex-col items-center">
+                <section className="mx-auto container p-5 flex flex-col place-content-center">
                     <h1 className="text-center text-xl font-semibold">No wishlisted Items</h1>
                 <img src={image} className="object-cover mx-auto mt-10"/>
                 <button className="bg-green-400 px-5 py-3 text-xl rounded-lg text-gray-700 font-semibold mt-10 cursor-pointer hover:bg-green-500 active:bg-green-600" onClick={()=> navigate("/")}>Explore Items</button>
@@ -29,7 +29,7 @@ function WishlistPage() {
         <main className="min-h-screen bg-gray-50">
             <section className="mx-auto container p-5">
                 <h1 className="text-xl text-gray-700 font-semibold w-full mt-5">Wishlist</h1>
-<article className="flex flex-col items-center md:flex-row gap-10 mt-10">
+<article className="flex flex-col md:flex-row gap-10 mt-10">
                     {items.map((item) => 
                         <WishlistCard key={item.id} wishListItem={item}/>
                     )}
